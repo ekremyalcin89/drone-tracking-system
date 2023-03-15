665301552e3230892a09b0052abc85fe6f1ba81f
ekrem <yalcinekrem89@gmail.com>
2023-03-15 15:43:20 +0300

    controllers added

--- drone tracking system/SigninVC.swift
+++ drone tracking system/SigninVC.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class SigninVC: UIViewController {
 
+    @IBOutlet weak var yollaLabel: UILabel!
+    
+    
+    @IBOutlet weak var usernameLabel: UITextField!
+    
+    
+    @IBOutlet weak var passwordLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func forgotPasswordLabel(_ sender: Any) {
+    }
+    
+    @IBAction func signinLabel(_ sender: Any) {
+        performSegue(withIdentifier: "toListVC", sender: nil)
+    }
+    
+    @IBAction func signupLabel(_ sender: Any) {
+    }
+    
+    
+    
+    
 }
 
